3c681a43224ff8d4de109b079b0baef828406b2c
niculbolas <105987416+niculbolas@users.noreply.github.com>
2022-11-01 20:42:27 -0400

    Deliverable 3 completed.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -22,3 +22,23 @@ total_summary <- coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), V
 # Summarize the lot data
 lot_summary <- coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median  = median(PSI), Variance = var(PSI), SD = sd(PSI))
 
+# Do a T-Test on all the lot data
+total_T_test <- t.test(coil_df$PSI, mu = 1500)
+
+# Do a lot by lot T-Test
+Lot_1 <- coil_df %>% subset(Manufacturing_Lot == "Lot1")
+Lot_2 <- coil_df %>% subset(Manufacturing_Lot == "Lot2")
+Lot_3 <- coil_df %>% subset(Manufacturing_Lot == "Lot3")
+
+ttest_1 <- t.test(Lot_1$PSI, mu = 1500)
+ttest_2 <- t.test(Lot_2$PSI, mu = 1500)
+ttest_3 <- t.test(Lot_3$PSI, mu = 1500)
+
+# Print statements to make images from
+total_T_test
+
+ttest_1
+
+ttest_2
+
+ttest_3
